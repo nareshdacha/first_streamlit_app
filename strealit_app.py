533e97e43a00a9049254b38dee858a7e9d2a67e4
nareshdacha <naresh.dacha16@gmail.com>
2022-05-21 21:08:50 -0500

    Update strealit_app.py
    fix

--- strealit_app.py
+++ strealit_app.py
@@ -26,4 +26,3 @@ streamlit.dataframe(fruitvice_norm )
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-import snowflake.connector
